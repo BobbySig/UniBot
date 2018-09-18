94b683842d8a77dde31c976e3f399c3c2888f06b
Bobby Signor <signor.bobby@gmail.com>
2018-09-17 16:22:54 -0800

    Add rudimentary status response for web requests

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const Discord = require('discord.js');
 const SpoilerBot = require('@bobbysig/spoilerbot');
+const http = require('http');
 
 /* --- Load dotenv --- */
 if (process.env.NODE_ENV !== "production") {
@@ -9,12 +10,50 @@ if (process.env.NODE_ENV !== "production") {
 }
 
 /* --- Launch SpoilerBot --- */
-var client = new Discord.Client();
-client.on("ready", function() {
+var spoilerbot = new SpoilerBot(new Discord.Client());
+spoilerbot.client.on("ready", function() {
   console.log("SpoilerBot online.");
 });
-var bot = new SpoilerBot(client);
-client.login(process.env.SPOILERBOT_SECRET).catch(function(reason) {
+spoilerbot.client.login(process.env.SPOILERBOT_SECRET).catch(function(reason) {
   console.error('Error: Discord login failed. Log:');
   console.error(reason);
 });
+
+/* --- Web server that returns status --- */
+http.createServer(function (req, res) {
+  res.setHeader('Content-Type', 'application/json');
+  res.write(JSON.stringify(status()));
+  res.end();
+}).listen(8080);
+
+/* --- Status Function --- */
+function status() {
+  let status = {};
+  status.UniBot = "ONLINE";
+  status = discordClientStatus(status, spoilerbot.client);
+  return status;
+}
+
+function discordClientStatus(status, client) {
+  switch(client.status) {
+    case Discord.Constants.Status.READY:
+      status.SpoilerBot = "READY"
+      break;
+    case Discord.Constants.Status.CONNECTING:
+      status.SpoilerBot = "CONNECTING"
+      break;
+    case Discord.Constants.Status.RECONNECTING:
+      status.SpoilerBot = "RECONNECTING"
+      break;
+    case Discord.Constants.Status.IDLE:
+      status.SpoilerBot = "IDLE"
+      break;
+    case Discord.Constants.Status.NEARLY:
+      status.SpoilerBot = "NEARLY"
+      break;
+    case Discord.Constants.Status.DISCONNECTED:
+      status.SpoilerBot = "DISCONNECTED"
+      break;
+  }
+  return status;
+}
